751fba4a784c1199d76f35b27b14f7b938743a9e
Wpjwwwww <15249697158@163.com>
2020-12-02 15:22:12 +0800

    Update week02.go

--- Week02/week02.go
+++ Week02/week02.go
@@ -1,6 +1,35 @@
 git@github.com:Wpjwwwww/Go-000.git
 package main
 import (
-
+"database/sql"
+"sql"
+"runtime"
 )
-exit
+func main(){
+  db,err:=sql.Open("查询的数据库名","数据库地址")
+  if err!=nil{
+    fmt.Println(err)
+  }
+  defer db.close()
+  type User struct{
+  Id int32
+  Name string
+  Age int8
+  }
+  var user User
+  err=db.QueryRow(
+  'SELECT id,name,age WHERE id=?',2
+  ).Scan(&user.Id,&user.Name,&user.Age)
+  switch{
+    case err==sql.ErrNoRows:
+    case err !=nil:
+    if _,file,line,ok :=runtime.Caller(0);
+    ok{
+      fmt.Println(err,file,line)}
+  }
+}
+//总结：Wraps erros时，1.错误要被日志记录；2.应用程序处理错误，要保证100%的完整性；3.之后不再报告当前错误；
+
+
+
+
